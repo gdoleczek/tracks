4bb87635316625d1d7d2bce01281d064b38b92ea
Grzegorz Doleczek <g.dolecze@pixelmeal.com>
2014-02-22 23:38:08 +0100

    Finished stage5

--- app/models/stats/tag_cloud.rb
+++ app/models/stats/tag_cloud.rb
@@ -14,16 +14,10 @@ class TagCloud
   def compute
     levels=10
 
-    query = "SELECT tags.id, name, count(*) AS count"
-    query << " FROM taggings, tags, todos"
-    query << " WHERE tags.id = tag_id"
-    query << " AND taggings.taggable_id = todos.id"
-    query << " AND todos.user_id="+user.id.to_s+" "
-    query << " AND taggings.taggable_type='Todo' "
-    query << " GROUP BY tags.id, tags.name"
-    query << " ORDER BY count DESC, name"
-    query << " LIMIT 100"
-    @tags = Tag.find_by_sql(query).sort_by { |tag| tag.name.downcase }
+    params = [sql, user.id]
+    @tags = Tag.find_by_sql(
+      params
+    ).sort_by { |tag| tag.name.downcase }
 
     max, @min = 0, 0
     @tags.each { |t|
@@ -33,19 +27,9 @@ class TagCloud
 
     @divisor = ((max - @min) / levels) + 1
 
-    query = "SELECT tags.id, tags.name AS name, count(*) AS count"
-    query << " FROM taggings, tags, todos"
-    query << " WHERE tags.id = tag_id"
-    query << " AND todos.user_id=? "
-    query << " AND taggings.taggable_type='Todo' "
-    query << " AND taggings.taggable_id=todos.id "
-    query << " AND (todos.created_at > ? OR "
-    query << "      todos.completed_at > ?) "
-    query << " GROUP BY tags.id, tags.name"
-    query << " ORDER BY count DESC, name"
-    query << " LIMIT 100"
+    params =  [sql(@cut_off), user.id, @cut_off, @cut_off]
     @tags_90days = Tag.find_by_sql(
-      [query, user.id, @cut_off, @cut_off]
+      params
     ).sort_by { |tag| tag.name.downcase }
 
     max_90days, @min_90days = 0, 0
@@ -55,6 +39,23 @@ class TagCloud
     }
 
     @divisor_90days = ((max_90days - @min_90days) / levels) + 1
+  end
 
+  private
+
+  def sql(cut_off = nil)
+    query = "SELECT tags.id, tags.name AS name, count(*) AS count"
+    query << " FROM taggings, tags, todos"
+    query << " WHERE tags.id = tag_id"
+    query << " AND todos.user_id=? "
+    query << " AND taggings.taggable_type='Todo' "
+    query << " AND taggings.taggable_id=todos.id "
+    if cut_off
+      query << " AND (todos.created_at > ? OR "
+      query << "      todos.completed_at > ?) "
+    end
+    query << " GROUP BY tags.id, tags.name"
+    query << " ORDER BY count DESC, name"
+    query << " LIMIT 100"
   end
 end
